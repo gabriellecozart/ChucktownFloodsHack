f4949f8dc93dcd3b7c59d72fb9fee0bc998639fe
Gabrielle <gabriellecozart@yahoo.com>
2018-08-24 22:29:48 -0400

    created leaflet map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import L from 'leaflet';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   constructor(public navCtrl: NavController) {
+    
+  }
 
+  ionViewDidLoad() {
+    this.loadMap();
+   
+  }
+
+  loadMap() {
+    this.map = L.map("map").setView([32.797008, -79.945267], 13);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+    }).addTo(this.map);
+
+    
   }
 
 }
